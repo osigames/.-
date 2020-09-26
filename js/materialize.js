46bc26a9e50ef0a083e2d22b0adc879198e3d4c2
osigames <71914066+osigames@users.noreply.github.com>
2020-09-26 03:36:25 +0000

    Add files via upload

---
+++ js/materialize.js
@@ -0,0 +1,25 @@
+const onglets = document.querySelectorAll('.onglets');
+const contenu = document.querySelectorAll('.contenu');
+let index = 0;
+
+onglets.forEach(onglet =>{
+
+  onglet.addEventListener('click' , () => {
+
+    if (onglet.classList.contains('active')) {
+      return;
+    } else {
+      onglet.classList.add('active');
+    }
+
+    index = onglet.getAttribute('data-anim');
+    console.log(index);
+
+    for (i = 0; i < onglets.length; i++) {
+      
+      if(onglets[i].getAttribute('data-anim')
+    != index)
+    }
+  })
+
+})
